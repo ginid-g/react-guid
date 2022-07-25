2cd5775856f34f37674571fac62f9bd932f8e99c
Ginid Thakur <ginid78@gmail.com>
2022-07-25 18:33:48 +0530

    add expense

--- src/sub/expense/ExpensProject.js
+++ src/sub/expense/ExpensProject.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import ExpensList from "./ExpensList";
+import ExpenseForm from "../newExpense/ExpenseForm";
 
 function ExpensProject() {
   const expenses = [
@@ -29,29 +30,38 @@ function ExpensProject() {
     },
   ];
 
+  const addExpense = (expenseData) => {
+    console.log(expenseData);
+  };
+
   return (
-    <Card>
-      <ExpensList
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpensList
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpensList
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpensList
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
-    </Card>
+    <>
+      <Card>
+        <ExpenseForm onSave={addExpense}></ExpenseForm>
+      </Card>
+      <Card>
+        <ExpensList
+          title={expenses[0].title}
+          date={expenses[0].date}
+          amount={expenses[0].amount}
+        />
+        <ExpensList
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
+        <ExpensList
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+        <ExpensList
+          title={expenses[3].title}
+          date={expenses[3].date}
+          amount={expenses[3].amount}
+        />
+      </Card>
+    </>
   );
 }
 
---
+++ src/sub/newExpense/ExpenseForm.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const ExpenseForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const handleTitle = (event) => setTitle(event.target.value);
+  const handleDate = (event) => setDate(event.target.value);
+  const handleAmount = (event) => setAmount(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSave({
+      title: title,
+      date: date,
+      amount: amount,
+    });
+
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label for="title">Title</label>
+          <input type="text" id="title" value={title} onChange={handleTitle} />
+        </div>
+
+        <div>
+          <label for="amount">Amount</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </div>
+
+        <div>
+          <label for="date">date</label>
+          <input type="date" id="date" value={date} onChange={handleDate} />
+        </div>
+
+        <div>
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ExpenseForm;
