b1be36e20d834bb522200fc9972fd45d6736307d
Ginid Thakur <ginid78@gmail.com>
2022-07-27 21:32:56 +0530

    list in react

--- src/sub/expense/ExpensProject.js
+++ src/sub/expense/ExpensProject.js
@@ -1,37 +1,41 @@
+import { useState } from "react";
 import Card from "../UI/Card";
-import ExpensList from "./ExpensList";
+
 import ExpenseForm from "../newExpense/ExpenseForm";
+import Expense from "./Expense";
+
+const initial_expneses = [
+  {
+    id: 1,
+    title: "Some title",
+    amount: 25.65,
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Some title2",
+    amount: 25.65,
+    date: new Date(),
+  },
+  {
+    id: 3,
+    title: "Some title3",
+    amount: 25.65,
+    date: new Date(),
+  },
+  {
+    id: 4,
+    title: "Some title4",
+    amount: 25.65,
+    date: new Date(),
+  },
+];
 
 function ExpensProject() {
-  const expenses = [
-    {
-      id: 1,
-      title: "Some title",
-      amount: 25.65,
-      date: new Date(),
-    },
-    {
-      id: 2,
-      title: "Some title2",
-      amount: 25.65,
-      date: new Date(),
-    },
-    {
-      id: 3,
-      title: "Some title3",
-      amount: 25.65,
-      date: new Date(),
-    },
-    {
-      id: 4,
-      title: "Some title4",
-      amount: 25.65,
-      date: new Date(),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initial_expneses);
 
   const addExpense = (expenseData) => {
-    console.log(expenseData);
+    setExpenses((prev) => [...prev, expenseData]);
   };
 
   return (
@@ -40,26 +44,7 @@ function ExpensProject() {
         <ExpenseForm onSave={addExpense}></ExpenseForm>
       </Card>
       <Card>
-        <ExpensList
-          title={expenses[0].title}
-          date={expenses[0].date}
-          amount={expenses[0].amount}
-        />
-        <ExpensList
-          title={expenses[1].title}
-          date={expenses[1].date}
-          amount={expenses[1].amount}
-        />
-        <ExpensList
-          title={expenses[2].title}
-          date={expenses[2].date}
-          amount={expenses[2].amount}
-        />
-        <ExpensList
-          title={expenses[3].title}
-          date={expenses[3].date}
-          amount={expenses[3].amount}
-        />
+        <Expense expenses={expenses} />
       </Card>
     </>
   );
