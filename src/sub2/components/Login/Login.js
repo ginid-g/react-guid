01960e1785f3d1e003bdd62f7c7031696292e2eb
Ginid Thakur <ginid78@gmail.com>
2022-08-05 14:33:42 +0530

    bug fix

--- src/sub2/components/Login/Login.js
+++ src/sub2/components/Login/Login.js
@@ -12,9 +12,15 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const indentifire = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 300);
+
+    return () => {
+      clearTimeout(indentifire);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
