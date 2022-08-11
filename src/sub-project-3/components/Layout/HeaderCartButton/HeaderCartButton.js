b7d02bd8ae4abb66dce1a3d92b71834a64fba085
Ginid Thakur <ginid78@gmail.com>
2022-08-11 17:44:34 +0530

    sub project 3 --completed

--- src/sub-project-3/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/sub-project-3/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,15 +1,27 @@
+import { useContext } from "react";
+
+import CartContext from "../../../store/cart-context";
+
 import CartIcon from "../../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const cartContext = useContext(CartContext);
+
+  const items = cartContext.items.map((x) => x.amount);
+
+  const numberOfItems = items.reduce((a, b) => {
+    return a + b;
+  }, 0);
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 }
--- src/sub-project-3/components/Meals/MealItem/MealItemForm.js
+++ src/sub-project-3/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,20 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 function MealItemForm(props) {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enterAmount = amountInputRef.current.value;
+    const enterAmountNumber = +enterAmount;
+
+    props.onAddToCart(enterAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
         type="number"
@@ -12,6 +23,7 @@ function MealItemForm(props) {
         max="5"
         step="1"
         defaultValue="1"
+        ref={amountInputRef}
       />
       <button>+ Add</button>
     </form>
