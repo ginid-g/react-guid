87f45308a0e3536a598e6e32d866a94f18ef02b9
Ginid Thakur <ginid78@gmail.com>
2022-08-29 18:31:10 +0530

    react fpr,s

---
+++ src/sub-project-6/Main.js
@@ -0,0 +1,12 @@
+import SimpleInput from "./components/SimpleInput";
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default Main;
